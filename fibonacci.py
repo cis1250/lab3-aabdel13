2bf5630a08eba00a2cb9567c681f6184a60dff13
Ammar Abdelazim <aabdel13@uoguelph.ca>
2025-09-29 12:50:00 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,4 @@
+# keep asking untill the user enters a valid positive integer
 while True:
   user_input = input("enter the number of terms in the Fibonacci sequance: ")
   
@@ -12,7 +13,8 @@ while True:
      print("please enter a positive integer.")
       
 a, b = 0, 1
+# generate and print the Fibonacci sequence using a for loop
 for _ in range(n):
   print(a, end=" ")
   a, b = b, a + b
-print()
+print() # move to a new line after printing the sequence
